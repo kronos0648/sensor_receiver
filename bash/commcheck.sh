83bf1a36c5092a6058946738c0ed364e0c3d562f
kronos0648 <dkstjdgus15@gmail.com>
2023-07-10 22:30:46 +0900

    Edit Comments

--- bash/commcheck.sh
+++ bash/commcheck.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
-
-#refenence link : https://blog.dalso.org/article/bash-%EC%89%98-%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8%EB%A1%9C-%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC-%EC%9E%A5%EC%B9%98%EB%AA%85-ip-mac-%EC%A3%BC%EC%86%8C
-
-
-
-#Network Check Logic
-######
 num_tried=1
-
-
 DEV=""
 IP=""
 MAC=""
 
+#네트워크 검사 최대 20회까지 시행
 while [ -z ${IP} ] && [ ${num_tried} -lt 21 ]
 do
+    #네트워크 인터페이스 탐지
     DEV_LIST=$(ip route show default | awk '/default/{print $5}')
     DEV_ISEXIST=0
     for ((i=1;i<100;i++)) do
         DEV_ITEM=$(echo $DEV_LIST | awk '{print $'$i'}')
 
+        #감지된 네트워크 인터페이스가 없을 때
         if [ ${#DEV_ITEM} -eq 0 ]; then
             DEV=${DEV_ITEM}
             DEV_ISEXIST=0
             break
         fi
 
+        #감지된 네트워크 인터페이스가 무선 랜일 때
         if [ ${DEV_ITEM} == "wlan0" ]; then
             DEV=${DEV_ITEM}
             DEV_ISEXIST=1
@@ -36,10 +31,11 @@ do
 
     echo -e "\n\n"
 
+    #감지된 네트워크 인터페이스가 없을 때
     if [ 0 -eq ${DEV_ISEXIST} ]; then
         echo "Network Detection Trial ${num_tried} / 20"
         echo "Network Interface Not Found"
-        #CHECK per 3 sec
+        #시행 횟수를 늘려가며 3초마다 탐지 반복
         num_tried=`expr $num_tried + 1`
         sleep 3
         continue
@@ -48,9 +44,10 @@ do
         echo "Network Interface Found!"
     fi
 
-
+    #IP 주소 탐지
     IP=$(ip -4 -o addr show ${DEV} | awk '{print $4}')
     IP=$(echo $IP | cut -d '/' -f1)
+    # 할당된 IP가 아닌 loopback IP일 경우 => IP 할당 안 된 상태
     if [ ${IP} == "127.0.0.1" ]; then
         echo "IP Address Not Found"
         #CHECK per 3 sec
@@ -59,13 +56,12 @@ do
         continue
     fi
 
-
+    #MAC Address 할당 여부 검사
     MAC=$(cat /sys/class/net/${DEV}/address)
     if [ -z ${MAC} ]; then
         echo "mac address not found"
         continue
     fi
-    
     exit 0
 done
 
